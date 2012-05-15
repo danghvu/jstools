3d70499382fa70f578d8ee579baf7362b9ade659
danghvu <danghvu@gmail.com>
2012-05-15 23:50:35 +0200

    1st working copy

--- bg.js
+++ bg.js
@@ -17,46 +17,47 @@ var libraries = {
     crypto: 'http://www.res.vudang.com/jsconsole_crypto/crypto.js',
 };
 
-function init_lib() {
-    localStorage["lib"] = JSON.stringify(libraries);
-    var load_lib = libraries;
-    for (key in load_lib) 
-        load_lib[key] = false;
-    localStorage["load_lib"] = JSON.stringify(load_lib);
-}
-
 var settings = {
-    get def_lib() {
-        return libraries;
-    },
     get lib() {
-        if (!localStorage["lib"]) 
-            init_lib();
-        return localStorage["lib"];
+        if (!this._lib)
+            this._lib = libraries;
+        return this._lib;
     },
     set lib(val) {
-        localStorage["lib"] = val;
+        lib = val;
     },
-    set load_lib(lib) {
-        if (localStorage["load_lib"])         
-            var check_lib = JSON.parse(localStorage["load_lib"]);
-       
-        check_lib[lib] = true;
-        localStorage["load_lib"] = JSON.stringify(check_lib);
+    get checked() {
+        if (!this._checked)
+            this._checked = {};
+        return this._checked;
     },
-    get load_lib() {
-        if (!localStorage["load_lib"])
-            init_lib();
-        return localStorage["load_lib"];
-    }
+}
+
+settings.save = function() {
+    localStorage["load_lib"] = JSON.stringify(this._checked);
+    localStorage["lib"] = JSON.stringify(this._lib);
+}
+
+settings.load = function() {
+    this._checked = JSON.parse(localStorage["load_lib"]);
+    this._lib = JSON.parse(localStorage["lib"]);
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
     if (DEBUG) console.debug(request.query);
+    settings.load();
 
     switch( request.query ) {
         case "load_settings": 
-            sendResponse({lib: settings.lib, load_lib:settings.load_lib});
+            sendResponse({lib: settings.lib, load_lib:settings.checked});
+            break;
+        case "settings_loadlib":
+            settings._checked[request.data.id] = true;
+            settings.save();
+            break;
+        case "settings_unloadlib":
+            settings._checked[request.data.id] = false;
+            settings.save();
             break;
         default:
             console.log("Invalid Response", request, sender);
--- popup.js
+++ popup.js
@@ -1,32 +1,34 @@
-function load_js ( lib ) {
-    console.log("add " + lib);
-}
-
-function unload_js ( lib ) {
-    console.log("remove " + lib);
+function sendquery_selectedtab( query , data ) {
+    chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.sendRequest(tab.id, {query: query, data:data }, function(response) {});
+    });
 }
 
 function update_state( e ) {
     var lib = e.data.lib;
     var load_lib = e.data.load_lib;
-    if ($(this).is(":checked")) 
-        load_js(lib[load_lib]);
-    else
-        unload_js(lib[load_lib]);
+    if ($(this).is(":checked")) {
+        sendquery_selectedtab( "load_js", {url:lib[load_lib], id:load_lib} );
+        chrome.extension.sendRequest({query:"settings_loadlib", data:{id:load_lib}}, function(response){});
+    }
+    else {
+        sendquery_selectedtab( "unload_js", {url:lib[load_lib], id:load_lib} ); 
+        chrome.extension.sendRequest({query:"settings_unloadlib", data:{id:load_lib}}, function(response){});
+    }
 }
 
 $( function() {
     chrome.extension.sendRequest({query:"load_settings"}, function (response){
-        lib = JSON.parse(response.lib);
-        load_lib = JSON.parse(response.load_lib);
+        lib = response.lib;
+        load_lib = response.load_lib;
 
         for (var key in lib) {
-            console.log(key,lib[key], load_lib[key]);
             var checked="";
             if (load_lib[key])
                 checked="checked"; 
+            console.log( key, load_lib[key] );
 
-            $("#options").append("<input type=\"checkbox\" id=\"item_"+ key +"\" value=\"" + key +" " + checked + "\"/>" + key + "<br/>");
+            $("#options").append("<input type=\"checkbox\" id=\"item_"+ key +"\" value=\"" + key +"\" " + checked + "/>" + key + "<br/>");
             $("#item_"+key).change( { lib:lib, load_lib :key} , update_state );
         }
     });
